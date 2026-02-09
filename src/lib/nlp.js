643f831a1089ac303d5f7abc7dec1089b1940940
Airbus-A330 <united.vietnam.airlines@gmail.com>
2026-02-09 13:34:00 -0500

    feat(nlp): flesh out normalization layer part 01
    Layer in more of the local text normalization and label extraction flow (part 1/25).

--- src/lib/nlp.js
+++ src/lib/nlp.js
@@ -1,5 +1,11 @@
-module.exports = {
-  analyzeAffect(rawText) {
-    return { rawText, normalizedText: String(rawText || '') };
-  },
-};
+/**
+ * @file nlp.js
+ * @description Local NLP helpers for turning short survey text into normalized
+ * emotions, topics, valence, arousal, and quality diagnostics.
+ */
+
+const natural = require('natural');
+const { removeStopwords } = require('stopword');
+const { EMOTION_TAXONOMY, TOPIC_TAXONOMY } = require('./emotion-taxonomy');
+
+const tokenizer = new natural.WordTokenizer();
