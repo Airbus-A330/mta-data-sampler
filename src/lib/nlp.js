25526d7e8f82ae86899373178b2888cca8feed95
Yazzie55555 <yasmin.hassoobh@gmail.com>
2026-02-09 21:45:00 -0500

    feat(nlp): flesh out normalization layer part 02
    Layer in more of the local text normalization and label extraction flow (part 2/25).

--- src/lib/nlp.js
+++ src/lib/nlp.js
@@ -9,3 +9,13 @@ const { removeStopwords } = require('stopword');
 const { EMOTION_TAXONOMY, TOPIC_TAXONOMY } = require('./emotion-taxonomy');
 
 const tokenizer = new natural.WordTokenizer();
+const stemmer = natural.PorterStemmer;
+const sentimentAnalyzer = new natural.SentimentAnalyzer('English', stemmer, 'afinn');
+
+const MOJIBAKE_REPLACEMENTS = new Map([
+  ['â€™', "'"],
+  ['â€œ', '"'],
+  ['â€\u009d', '"'],
+  ['â€“', '-'],
+  ['â€”', ' - '],
+  ['â€¦', '...'],
